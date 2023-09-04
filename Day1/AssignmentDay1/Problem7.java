09997209959b66e18ff46905d7c0693db561f76b
panwarvishal49 <100397743+panwarvishal49@users.noreply.github.com>
2023-09-04 22:46:22 +0530

    Update Problem7.java

--- Day1/AssignmentDay1/Problem7.java
+++ Day1/AssignmentDay1/Problem7.java
@@ -5,17 +5,27 @@ public class Problem7 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter number :"); 
+		boolean count=false;
 		int num=sc.nextInt();//getting input
 		
 		int hours=num/100;
-		if(hours>12) {
-			hours-=12;
+		if(hours==0)
+		{
+			hours=12;
 		}
-		
+		else if(hours>=12) {
+			if(hours>12) {
+				hours-=12;
+			}
+			count=true;
+		}
 		int minutes=num%100;
-		
-		System.out.printf("time in 12 hours format = %d : %d",hours,minutes);//12 hours time format
-		
+		if(count) {
+			System.out.printf("time in 12 hours format = %d : %d pm",hours,minutes);//12 hours time format
+		}
+		else {
+			System.out.printf("time in 12 hours format = %d : %d am",hours,minutes);//12 hours time format
+		}
 		sc.close();
 	}
 
