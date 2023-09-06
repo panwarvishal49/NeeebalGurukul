5b32b3d2d1a42c1d75aebf800fdf11afd57db324
panwarvishal49 <100397743+panwarvishal49@users.noreply.github.com>
2023-09-06 10:31:57 +0530

    Add files via upload

---
+++ Day2/AssignmentDay2/Problem8.java
@@ -0,0 +1,36 @@
+package AssignmentDay2;
+
+public class Problem8 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				if(j==i) {
+					System.out.print(" *");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			if(i==1||i==5)
+			{
+				for(int k=1;k<=8;k++)
+				{
+					System.out.print(" *");
+				}
+			}
+			else {
+				for(int k=1;k<=8;k++)
+				{
+					System.out.print("  ");
+				}
+			}
+			System.out.print(" *");
+			System.out.println();
+		}
+
+	}
+
+}
